5958d7d6c44e25f6d5040d29c7260e504ee2bb58
Mukhamed Khasanza <fragilehm2411@gmail.com>
2022-07-07 20:20:02 +0600

    Added multi print to example.

---
+++ example/src/MultiPrint.tsx
@@ -0,0 +1,176 @@
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useEffect } from 'react';
+import EscPosPrinter, {
+  getPrinterSeriesByName,
+} from 'react-native-esc-pos-printer';
+
+import { useState } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import type { IPrinter } from 'lib/typescript';
+
+type Props = {
+  closeModal: () => void;
+};
+const MultiPrint = ({ closeModal }: Props) => {
+  const backgroundStyle = {
+    margin: 16,
+  };
+
+  const [printers, setPrinters] = useState<IPrinter[]>([]);
+
+  useEffect(() => {
+    handleDiscover();
+  }, []);
+
+  const handleDiscover = async () => {
+    const discoveredPrinters = await EscPosPrinter.discover();
+    setPrinters(discoveredPrinters);
+  };
+
+  const handleInit = async (printer: IPrinter) => {
+    try {
+      await EscPosPrinter.instantiate({
+        target: printer.target,
+        seriesName: getPrinterSeriesByName(printer.name),
+        language: 'EPOS2_LANG_EN',
+      });
+    } catch (error) {
+      // Init error
+    }
+  };
+
+  const handleConnect = (printer: IPrinter) => {
+    EscPosPrinter.connect(printer.target);
+  };
+
+  const handleDisconnect = (printer: IPrinter) => {
+    EscPosPrinter.disconnectPrinter(printer.target);
+  };
+
+  const handlePrint = async (printer: IPrinter) => {
+    const printing = new EscPosPrinter.printing();
+    try {
+      await printing
+        .initialize()
+        .align('center')
+        .size(3, 3)
+        .line(`Print target: ${printer.target}`)
+        .cut()
+        .send({
+          target: printer.target,
+        });
+    } catch (error) {
+      // Printing error
+    }
+  };
+
+  const handlePrintAll = () => {
+    const printRequests = printers.map((printer) => handlePrint(printer));
+    Promise.all(printRequests);
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}
+      >
+        <TouchableOpacity
+          style={{
+            marginBottom: 24,
+          }}
+          onPress={closeModal}
+        >
+          <Text style={{ color: '#0A84FF' }}>Close</Text>
+        </TouchableOpacity>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <TouchableOpacity onPress={handleDiscover}>
+            <Text>Discover printers</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handlePrintAll}>
+            <Text>Print all</Text>
+          </TouchableOpacity>
+        </View>
+        {printers.map((printer) => {
+          return (
+            <View
+              style={{
+                padding: 12,
+                borderWidth: 1,
+                marginVertical: 12,
+                borderRadius: 4,
+                borderColor: 'grey',
+              }}
+            >
+              <Text>{printer.name}</Text>
+              <TouchableOpacity
+                style={{
+                  padding: 12,
+                  borderWidth: 1,
+                  marginVertical: 12,
+                  borderColor: '#23395d',
+                  borderRadius: 4,
+                }}
+                onPress={() => handleInit(printer)}
+              >
+                <Text>Init</Text>
+              </TouchableOpacity>
+              {/* <TouchableOpacity
+                style={{
+                  padding: 12,
+                  borderWidth: 1,
+                  marginVertical: 12,
+                  borderColor: '#23395d',
+                  borderRadius: 4,
+                }}
+                onPress={() => handleConnect(printer)}
+              >
+                <Text>Connect</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  padding: 12,
+                  borderWidth: 1,
+                  marginVertical: 12,
+                  borderColor: '#23395d',
+                  borderRadius: 4,
+                }}
+                onPress={() => handleDisconnect(printer)}
+              >
+                <Text>Disconnect</Text>
+              </TouchableOpacity> */}
+              <TouchableOpacity
+                style={{
+                  padding: 12,
+                  borderWidth: 1,
+                  marginVertical: 12,
+                  borderColor: '#23395d',
+                  borderRadius: 4,
+                }}
+                onPress={() => handlePrint(printer)}
+              >
+                <Text>Print</Text>
+              </TouchableOpacity>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default MultiPrint;
