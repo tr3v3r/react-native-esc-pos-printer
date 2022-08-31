584adb773d9517d59969b3c0257e1abc6cdf3000
Mukhamed Khasanza <fragilehm2411@gmail.com>
2022-08-31 15:07:31 +0600

    Add Android helper classes.

---
+++ android/src/main/java/com/reactnativeescposprinter/ThePrinterState.java
@@ -0,0 +1,9 @@
+package com.reactnativeescposprinter;
+
+public enum ThePrinterState {
+    PRINTER_IDLE,
+    PRINTER_CONNECTING,
+    PRINTER_DISCONNECTING,
+    PRINTER_PRINTING,
+    PRINTER_REMOVING
+}
---
+++ android/src/main/java/com/reactnativeescposprinter/ePOSCmd.java
@@ -0,0 +1,7 @@
+package com.reactnativeescposprinter;
+
+public class ePOSCmd {
+
+    public final static int POS_SUCCESS = 0;
+
+}
