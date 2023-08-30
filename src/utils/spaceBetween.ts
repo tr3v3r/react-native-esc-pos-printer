1359a68143fa260f4d9f0f65c8e449f98c6add55
Anatoly <anatoly.pashias6@gmail.com>
2023-08-30 12:58:38 +0300

    feat: no trim in ISpaceBetweenParams

--- src/utils/spaceBetween.ts
+++ src/utils/spaceBetween.ts
@@ -48,6 +48,7 @@ export const spaceBetween = (
     textToWrap = 'left',
     textToWrapWidth,
     gapSymbol = ' ',
+    noTrim,
   }: ISpaceBetweenParams
 ): string => {
   const { leftTextWidth, rightTextWidth } = getTextsWidths(
@@ -62,7 +63,7 @@ export const spaceBetween = (
     .wrap(left, {
       width: leftTextWidth,
       break: true,
-      noTrim: leftTextWidth >= left.length,
+      noTrim,
     })
     .split('\n');
 
@@ -70,7 +71,7 @@ export const spaceBetween = (
     .wrap(right, {
       width: rightTextWidth,
       break: true,
-      noTrim: rightTextWidth >= right.length,
+      noTrim,
     })
     .split('\n');
 
