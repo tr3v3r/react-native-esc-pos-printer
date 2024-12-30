dfd98dcd4266b26a1ed8e9d5af8ca0312e12dd2a
Aliaksei Astafyeu <lochost40@gmail.com>
2024-12-30 09:53:41 +0100

    fix(fixed): update example to the latest RN, update android package name to fix expo 52 build issue

--- src/core/utils/constants.ts
+++ src/core/utils/constants.ts
@@ -1,9 +1,12 @@
 export function remapConstants<T extends object>(
   constants: T
 ): Record<string, keyof T> {
-  return Object.keys(constants).reduce((acc, key) => {
-    const value = constants[key as keyof T];
-    acc[String(value)] = key as keyof T;
-    return acc;
-  }, {} as Record<string, keyof T>);
+  return Object.keys(constants).reduce(
+    (acc, key) => {
+      const value = constants[key as keyof T];
+      acc[String(value)] = key as keyof T;
+      return acc;
+    },
+    {} as Record<string, keyof T>
+  );
 }
